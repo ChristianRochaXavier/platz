3054c27d30c9c0bef04d3f406d1aa82e8f18c8d9
15153766 <15153766@Lab11-17.suicobrasileira.sp>
2016-06-10 16:36:20 -0300

    Auto completes dos ceps

---
+++ Platz 2.0/WebContent/js/eventoCEP.js
@@ -0,0 +1,79 @@
+$(document).ready(
+
+		function() {
+
+			// Variáveis
+			var txtCep = $("#form-evento\\:txtCep");
+			var rua = $("#input_form-evento\\:txtRua");
+			var bairro = $("#input_form-evento\\:txtBairro");
+			var cidade = $("#input_form-evento\\:txtCidade");
+			var estado = $("#input_form-evento\\:txtEstado");
+
+			function limparCep() {
+				// Limpa valores do formulário de cep.
+				rua.val("");
+				bairro.val("");
+				cidade.val("");
+				estado.val("");
+			}
+
+			// Quando o campo cep perde o foco.
+			txtCep.blur(function() {
+
+				// Nova variável "cep" somente com dígitos.
+				var cep = $(this).val().replace(/\D/g, '');
+
+				// Verifica se campo cep possui valor informado.
+				if (cep != "") {
+
+					// Expressão regular para validar o CEP.
+					var validacep = /^[0-9]{8}$/;
+
+					// Valida o formato do CEP.
+					if (validacep.test(cep)) {
+
+						// Preenche os campos com "..." enquanto
+						// consulta o webservice.
+						rua.val("...");
+						bairro.val("...");
+						cidade.val("...");
+						estado.val("...");
+
+						// Consulta o webservice viacep.com.br/
+						$.getJSON("//viacep.com.br/ws/" + cep
+								+ "/json/?callback=?", function(dados) {
+
+							if (!("erro" in dados)) {
+								// Atualiza os campos com os valores da
+								// consulta.
+								rua.val(dados.logradouro);
+								bairro.val(dados.bairro);
+								cidade.val(dados.localidade);
+								estado.val(dados.uf);
+
+								// Deixa os campos como apenas leitura
+								rua.attr('readonly', 'true');
+								bairro.attr('readonly', 'true');
+								cidade.attr('readonly', 'true');
+								estado.attr('readonly', 'true');
+
+							} // end if.
+							else {
+								// CEP pesquisado não foi encontrado.
+								limparCep();
+								alert("CEP não encontrado.");
+							}
+						});
+					} // end if.
+					else {
+						// cep é inválido.
+						limparCep();
+						alert("Formato de CEP inválido.");
+					}
+				} // end if.
+				else {
+					// cep sem valor, limpa formulário.
+					limparCep();
+				}
+			});
+		});
